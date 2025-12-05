dd037dbf13781fb76eb5d2d2355eff2fc43fb115
AxelThorAsp <axel.aspelund@gmail.com>
2025-12-05 16:28:27 +0000

    day 1 part 2

--- 25/01/01.py
+++ 25/01/01.py
@@ -2,25 +2,59 @@ import sys
 
 lines = list(map(lambda x: x.replace('\n',''), sys.stdin.readlines()))
 
-dial = 50
-count_zero = 0
 
-def do(rot):
-    global dial
-    global count_zero
-    dir, deg = rot[0], int(rot[1:])
-    if dial == 0:
-        count_zero += 1
-    assert deg in range(0,1001)
-    if dir == 'R':
-        dial = (dial + deg) % 100
-    elif dir == 'L':
-        dial = (dial - deg) % 100
-    else:
-        assert False
+class Part1:
+    def __init__(self):
+        self.dial = 50
+        self.count_zero = 0
 
-for l in lines:
-    do(l)
+    def do(self, rot):
+        dir, deg = rot[0], int(rot[1:])
+        if self.dial == 0:
+            self.count_zero += 1
+        assert deg in range(0,1001)
+        if dir == 'R':
+            self.dial = (self.dial + deg) % 100
+        elif dir == 'L':
+            self.dial = (self.dial - deg) % 100
+        else:
+            assert False
 
-print(count_zero)
+    def ans(self):
+        for l in lines:
+            self.do(l)
+        print(self.count_zero)
 
+p1 = Part1()
+p1.ans()
+
+class Part2:
+    def __init__(self):
+        self.dial = 50
+        self.count_zero = 0
+
+    def do(self, rot):
+        dir, deg = rot[0], int(rot[1:])
+        assert deg in range(0,1001)
+        if dir == 'R':
+            while deg > 0:
+                self.dial = (self.dial + 1) % 100
+                if self.dial == 0:
+                    self.count_zero += 1
+                deg -= 1
+        elif dir == 'L':
+            while deg > 0:
+                self.dial = (self.dial - 1) % 100
+                if self.dial == 0:
+                    self.count_zero += 1
+                deg -= 1
+        else:
+            assert False
+
+    def ans(self):
+        for l in lines:
+            self.do(l)
+        print(self.count_zero)
+
+p2 = Part2()
+p2.ans()
